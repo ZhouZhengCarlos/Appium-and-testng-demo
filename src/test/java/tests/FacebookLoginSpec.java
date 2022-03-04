8f8960e0723fedddfc84a099024baca4208feaa8
zhouzhengcarlos <carloszhou0@gmail.com>
2022-03-04 08:42:25 -0600

    Deleting code that isn't in the guide

--- src/test/java/tests/FacebookLoginSpec.java
+++ src/test/java/tests/FacebookLoginSpec.java
@@ -1,12 +1,10 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import pages.CalculatorApp;
 import pages.FacebookHomePage;
 import pages.FacebookLoginPage;
 
@@ -14,33 +12,11 @@ public class FacebookLoginSpec extends BaseClass{
 	
 	private FacebookLoginPage loginPage;
 	private FacebookHomePage homePage;
-	private CalculatorApp calcApp;
 	
 	@BeforeClass
 	public void init() {
 		this.loginPage = new FacebookLoginPage(driver);
 		this.homePage = new FacebookHomePage(driver);
-		//this.calcApp = new CalculatorApp();
-	}
-	
-	@Test
-	public void testOne(){
-		driver.get("https://www.google.com");
-		driver.findElement(By.name("q")).sendKeys("Elden Ring");
-	    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-		
-		System.out.println("Completed test one");
-	}
-	
-	@Test
-	public void testTwo(){
-		
-		calcApp.getButtonOne().click();
-		calcApp.getButtonSum().click();
-		calcApp.getButtonOne().click();
-		calcApp.getButtonOne().click();
-		calcApp.getButtonEqual().click();
-		System.out.println("Completed test three");
 	}
 	
 	@Test
@@ -52,22 +28,5 @@ public class FacebookLoginSpec extends BaseClass{
 		loginPage.getNotNowButton().click();
 		homePage.getHamburgerMenuButton().click();
 		Assert.assertTrue((homePage.getProfileButton().getText().contains("your name")));
-	}
-	@Test
-	public void LikeOwnProfilePic() throws InterruptedException {
-		loginPage.Login();
-
-		Thread.sleep(5000);
-	
-		String previousContext = driver.getContext();
-		driver.context("NATIVE_APP");
-		driver.findElementByXPath(".//android.widget.Button[@text='BLOCK']").click();
-		driver.context(previousContext);
-		homePage.getHamburgerMenuButton().click();
-		homePage.getProfileButton().click();
-		homePage.getProfilePic().click();
-		homePage.getSeeProfilePicButton().click();
-		
-	}
-	
+	}	
 }
